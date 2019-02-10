d83cacb2a00562bcd87154a73d1329b33142656c
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2019-02-10 12:09:51 +0900

    add build configuration files

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const nodeSrc  = path.resolve(__dirname, 'src/node')
+const nodeDist = path.resolve(__dirname, 'dist')
+const webSrc  = path.resolve(__dirname, 'src/web')
+const webDist = path.resolve(__dirname, 'static/js')
+
+const nodeConfig = {
+    mode: 'development',
+    target: 'node',
+    externals: [nodeExternals()],
+    entry: ['@babel/polyfill', nodeSrc + '/main.js'],
+    output: {
+        path: nodeDist,
+        filename: 'server.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    plugins: []
+};
+
+const webConfig = {
+    mode: 'development',
+    target: 'web',
+    entry: ['@babel/polyfill', webSrc + '/index.js'],
+    output: {
+        path: webDist,
+        filename: 'client.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    plugins: []
+};
+
+module.exports = [nodeConfig, webConfig];
